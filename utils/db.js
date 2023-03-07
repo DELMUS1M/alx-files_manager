22b780c76312a32570b7313e29635d0bc7aafa1a
funkycadet <ericsmithsonian2@gmail.com>
2023-03-07 12:05:02 +0100

    fix: resolve connection error to db

--- utils/db.js
+++ utils/db.js
@@ -3,16 +3,27 @@ import { MongoClient } from 'mongodb';
 const HOST = process.env.DB_HOST || 'localhost';
 const PORT = process.env.DB_PORT || 27017;
 const DATABASE = process.env.DB_DATABASE || 'files_manager';
-
 const url = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
   constructor() {
-    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
-    this.client.connect().then(() => {
-      this.db = this.client.db(`${DATABASE}`);
-    }).catch((err) => {
-      console.log(err);
+    //  this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    //    this.client.connect().then(() => {
+    //     this.db = this.client.db(`${DATABASE}`);
+    //   }).catch((err) => {
+    //     console.log(err);
+    //   });
+    // }
+
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        this.db = client.db(`${DATABASE}`);
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
+      } else {
+        console.log(err.message);
+        this.db = false;
+      }
     });
   }
 
