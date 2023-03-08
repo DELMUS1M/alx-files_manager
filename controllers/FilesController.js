aafd53a66b516eea5e0f573dbdea8e4eba1b02fd
funkycadet <ericsmithsonian2@gmail.com>
2023-03-08 10:29:03 +0100

    task: add route to get and list files

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 import Queue from 'bull';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 import { findUserIdByToken } from '../utils/auth';
 
 class FilesController {
@@ -71,6 +72,85 @@ class FilesController {
     return response.status(201).json({
       id: fileInserted.ops[0]._id, userId, name, type, isPublic, parentId,
     });
+  }
+
+  static async getShow(request, response) {
+    const token = request.headers['x-token'];
+    if (!token) {
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const keyID = await redisClient.get(`auth_${token}`);
+    if (!keyID) {
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectID(keyID) });
+    if (!user) {
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const idFile = request.params.id || '';
+
+    const fileDocument = await dbClient.db.collection('files').findOne({ _id: ObjectID(idFile), userId: user._id });
+    if (!fileDocument) {
+      return response.status(404).send({ error: 'Not found' });
+    }
+
+    return response.send({
+      id: fileDocument._id,
+      userId: fileDocument.userId,
+      name: fileDocument.name,
+      type: fileDocument.type,
+      isPublic: fileDocument.isPublic,
+      parentId: fileDocument.parentId,
+    });
+  }
+
+  static async getIndex(request, response) {
+    const token = request.headers['x-token'];
+    if (!token) {
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const keyID = await redisClient.get(`auth_${token}`);
+    if (!keyID) {
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const parentId = request.query.parentId || '0';
+    const pagination = request.query.page || 0;
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectID(keyID) });
+    if (!user) {
+      response.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const aggregationMatch = { $and: [{ parentId }] };
+    let aggregateData = [
+      { $match: aggregationMatch },
+      { $skip: pagination * 20 },
+      { $limit: 20 },
+    ];
+    if (parentId === 0) {
+      aggregateData = [{ $skip: pagination * 20 }, { $limit: 20 }];
+    }
+
+    const files = await dbClient.db.collection('files').aggregate(aggregateData);
+    const filesArray = [];
+    await files.forEach((item) => {
+      const fileItem = {
+        id: item._id,
+        userId: item.userId,
+        name: item.name,
+        type: item.type,
+        isPublic: item.isPublic,
+        parentId: item.parentId,
+      };
+      filesArray.push(fileItem);
+    });
+
+    return response.send(filesArray);
   }
 }
 
